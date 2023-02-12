58a5f159555a1e6a5423f5145e319d7cc3de24c5
Andrew Petro <andrew.petro@wisc.edu>
2023-02-12 12:00:44 -0600

    refactor: move game down into new Game component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,56 +1,15 @@
 import "./App.css";
 import Header from "../Header";
-import Prompt from "../Prompt";
-import SpellingAttemptForm from "../SpellingAttemptForm";
-import Statistics from "../Statistics";
-import { WORDS } from "../../data.js";
-import { sample } from "../../utils.js";
+import Game from "../Game";
 import React from "react";
 
 function App() {
-  const [targetWord, setTargetWord] = React.useState(() => sample(WORDS));
-  const [countCorrectSpellings, setCountCorrectSpellings] = React.useState(0);
-  const [correctSpellingStreakLength, setCorrectSpellingStreakLength] =
-    React.useState(0);
 
-  function speakPrompt(wordObject) {
-
-    let wordUtterance = new SpeechSynthesisUtterance(wordObject.word);
-    window.speechSynthesis.speak(wordUtterance);
-
-    let sentenceUtterance = new SpeechSynthesisUtterance(wordObject.inASentence);
-    window.speechSynthesis.speak(sentenceUtterance);
-
-    window.speechSynthesis.speak(wordUtterance);
-  }
-
-  function checkSpellingAttempt(attemptedSpelling) {
-    if (attemptedSpelling === targetWord.word) {
-      window.alert(
-        "You are technically correct! This is the best kind of correct!"
-      );
-      const newWordObject = (sample(WORDS));
-      setTargetWord(newWordObject);
-      setCountCorrectSpellings(countCorrectSpellings + 1);
-      setCorrectSpellingStreakLength(correctSpellingStreakLength + 1);
-      speakPrompt(newWordObject)
-    } else {
-      window.alert(
-        `Alas, "${attemptedSpelling}" is not the correct spelling of "${targetWord.WORDS}".`
-      );
-      setCorrectSpellingStreakLength(0);
-    }
-  }
 
   return (
     <div className="App">
       <Header />
-      <Prompt targetWordObject={targetWord} />
-      <SpellingAttemptForm attemptSpellingFunction={checkSpellingAttempt} />
-      <Statistics
-        countCorrectSpellings={countCorrectSpellings}
-        correctSpellingStreakLength={correctSpellingStreakLength}
-      />
+      <Game />
     </div>
   );
 }
